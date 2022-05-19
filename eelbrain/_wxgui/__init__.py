e197db77a220c9c1f0593f4dcdf42eeaba59d822
Jon Gauthier <jon@gauthiers.net>
2022-05-19 15:47:13 -0400

    safe optional import of wx

--- eelbrain/_wxgui/__init__.py
+++ eelbrain/_wxgui/__init__.py
@@ -1,18 +1,30 @@
 """Import wx through this module to modify up logging"""
+import logging
 from warnings import filterwarnings
 
+L = logging.getLogger(__name__)
+
 filterwarnings('ignore', message='Not importing directory .*', module='wx.*')
-import wx
+try:
+    import wx
+except ImportError as e:
+    logging.warn("wx import failed; GUI members will not be accessible",
+                 exc_info=e)
 
-# filter unnecessary warnings
-from .. import _config
-if _config.SUPPRESS_WARNINGS:
-    filterwarnings('ignore', category=wx.wxPyDeprecationWarning)
-    filterwarnings('ignore', 'NewId()', category=DeprecationWarning)
-    filterwarnings('ignore', module='(traitsui|pyface|apptools)', category=DeprecationWarning)
-    filterwarnings('ignore', 'invalid escape sequence', DeprecationWarning)  # tvtk
-    wx.Log.EnableLogging(False)
+    wx = None
+    Icon = show_text_dialog = None
+    needs_jumpstart = get_app = run = None
+    history = select_epochs = select_components = load_stcs = None
+else:
+    # filter unnecessary warnings
+    from .. import _config
+    if _config.SUPPRESS_WARNINGS:
+        filterwarnings('ignore', category=wx.wxPyDeprecationWarning)
+        filterwarnings('ignore', 'NewId()', category=DeprecationWarning)
+        filterwarnings('ignore', module='(traitsui|pyface|apptools)', category=DeprecationWarning)
+        filterwarnings('ignore', 'invalid escape sequence', DeprecationWarning)  # tvtk
+        wx.Log.EnableLogging(False)
 
-from .utils import Icon, show_text_dialog
-from .app import needs_jumpstart, get_app, run
-from . import history, select_epochs, select_components, load_stcs
+    from .utils import Icon, show_text_dialog
+    from .app import needs_jumpstart, get_app, run
+    from . import history, select_epochs, select_components, load_stcs
